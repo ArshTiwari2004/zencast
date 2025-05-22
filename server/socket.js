c16600ed28f5da7b3ef58f4e73f8ec228b30c796
Arsh Tiwari <dchglucknow@gmail.com>
2025-05-23 04:19:44 +0530

    feat: add socket.js

---
+++ server/socket.js
@@ -0,0 +1,41 @@
+import { Server } from 'socket.io'
+import { redisClient } from './config/redis'
+
+const initializeSocket = (io) => {
+  io.on('connection', (socket) => {
+    console.log(`New socket connection: ${socket.id}`)
+
+    // Join room
+    socket.on('joinRoom', ({ roomId, userId }) => {
+      socket.join(roomId)
+      redisClient.sAdd(`room:${roomId}:participants`, userId)
+      io.to(roomId).emit('participantJoined', userId)
+    })
+
+    // WebRTC signaling
+    socket.on('offer', ({ to, offer }) => {
+      io.to(to).emit('offer', { from: socket.id, offer })
+    })
+
+    socket.on('answer', ({ to, answer }) => {
+      io.to(to).emit('answer', { from: socket.id, answer })
+    })
+
+    socket.on('ice-candidate', ({ to, candidate }) => {
+      io.to(to).emit('ice-candidate', { from: socket.id, candidate })
+    })
+
+    // Leave room
+    socket.on('disconnect', () => {
+      const rooms = Array.from(socket.rooms)
+      rooms.forEach(roomId => {
+        if (roomId !== socket.id) {
+          io.to(roomId).emit('participantLeft', socket.id)
+          redisClient.sRem(`room:${roomId}:participants`, socket.id)
+        }
+      })
+    })
+  })
+}
+
+export default initializeSocket
