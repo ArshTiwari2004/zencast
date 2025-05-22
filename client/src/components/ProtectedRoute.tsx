9d40c7d9bdd019ed6422749a2a2b35d3f372e8a1
Arsh Tiwari <dchglucknow@gmail.com>
2025-05-22 23:27:37 +0530

    feat: updated main.tsx and protected route

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { RootState } from '../store'
+import type { RootState } from '../store'
 
 export default function ProtectedRoute({ children }: { children: JSX.Element }) {
   const { user } = useSelector((state: RootState) => state.auth)
